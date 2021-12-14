b791f229dcdb092b6815f27516698c9d8f3a1d1e
tyrone <binktogether@gmail.com>
2021-12-14 03:27:34 -0800

    add land

--- contracts/IntoTheWilds.sol
+++ contracts/IntoTheWilds.sol
@@ -1,55 +1,107 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "./IEthemerals.sol";
 
+contract IntoTheWilds is ERC721Holder {
 
-contract IntoTheWilds {
+  /*///////////////////////////////////////////////////////////////
+                  STORAGE
+  //////////////////////////////////////////////////////////////*/
 
   address public admin;
 
   IEthemerals Merals;
 
-  mapping (uint256 => Land) private landPlots;
-    // mapping (uint256 => Loot);
+  mapping(uint8 => Land) public landPlots;
+  mapping(uint16 => mapping(uint16 => uint16)) public landClaimPoints;
+  mapping(uint256 => uint16[]) private defenders;
+  mapping(uint256 => uint16[]) private attackers;
+  mapping(uint256 => uint16[]) private looters;
+  mapping(uint256 => uint16[]) private birthers;
 
-  struct LootPool {
-    uint16 cost;
-    uint16[] lootType;
-    uint16[] DropRate;
+  enum Actions { DEFEND, ATTACK, LOOT, BIRTH, UNSTAKED }
+
+  struct Stake {
+    address owner;
+    uint8 landId;
+    uint88 timestamp;
+    Actions action;
   }
 
-  struct PetPool {
-    uint16 drainRate;
-    uint16[] petType;
-    uint16[] DropRate;
+  struct ItemPool {
+    uint8 cost;
+    uint8 drop1;
+    uint8 drop2;
+    uint8 drop3;
   }
 
   struct Land {
-    mapping(uint16 => uint16) landClaimPoints;
-    uint16 remainingELFx;
-    uint16 emissionRate; // DEV IMPROVE
-    LootPool lootPool;
-    PetPool petPool;
+    uint256 remainingELFx;
+    uint256 emissionRate; // DEV IMPROVE
+    ItemPool lootPool;
+    ItemPool petPool;
   }
 
-  // enum Actions { DEFEND, ATTACK, LOOT, BIRTH }
+  /*///////////////////////////////////////////////////////////////
+                  ADMIN FUNCTIONS
+  //////////////////////////////////////////////////////////////*/
 
-  // struct Action {
-  //   address owner;
-  //   uint88 timestamp;
-  //   Actions action;
-  // }
-
-
-  // CREATE INITIAL PLOTS
   constructor(address meralAddress) {
-      admin = msg.sender;
-      Merals = IEthemerals(meralAddress);
+
+    admin = msg.sender;
+    Merals = IEthemerals(meralAddress);
+
+    ItemPool memory loot1 = ItemPool({ cost: 10, drop1: 1, drop2: 2, drop3: 3 });
+    ItemPool memory pet1 = ItemPool({ cost: 10, drop1: 1, drop2: 2, drop3: 3 });
+
+    Land memory land1 = Land({ remainingELFx: 1000, emissionRate: 10, lootPool: loot1, petPool: pet1 });
+    Land memory land2 = Land({ remainingELFx: 1000, emissionRate: 10, lootPool: loot1, petPool: pet1 });
+    Land memory land3 = Land({ remainingELFx: 1000, emissionRate: 10, lootPool: loot1, petPool: pet1 });
+    Land memory land4 = Land({ remainingELFx: 1000, emissionRate: 10, lootPool: loot1, petPool: pet1 });
+    Land memory land5 = Land({ remainingELFx: 1000, emissionRate: 10, lootPool: loot1, petPool: pet1 });
+    Land memory land6 = Land({ remainingELFx: 1000, emissionRate: 10, lootPool: loot1, petPool: pet1 });
+
+    landPlots[1] = land1;
+    landPlots[2] = land2;
+    landPlots[3] = land3;
+    landPlots[4] = land4;
+    landPlots[5] = land5;
+    landPlots[6] = land6;
 
   }
 
+  function addLand(
+    uint8 id,
+    uint8 lootCost,
+    uint8 lootDrop1,
+    uint8 lootDrop2,
+    uint8 lootDrop3,
+    uint8 petCost,
+    uint8 petDrop1,
+    uint8 petDrop2,
+    uint8 petDrop3,
+    uint256 _remainingELFx,
+    uint256 _emissionRate) external
+  {
+    require(msg.sender == admin, "admin only");
 
+    Land memory land = Land({
+      remainingELFx: _remainingELFx,
+      emissionRate: _emissionRate,
+      lootPool: _addItemPool(lootCost, lootDrop1, lootDrop2, lootDrop3),
+      petPool: _addItemPool(petCost, petDrop1, petDrop2, petDrop3)
+    });
+    landPlots[id] = land;
+  }
 
+  /*///////////////////////////////////////////////////////////////
+                  INTERNAL FUNCTIONS
+  //////////////////////////////////////////////////////////////*/
+
+  function _addItemPool(uint8 _cost, uint8 _drop1, uint8 _drop2, uint8 _drop3) internal pure returns (ItemPool memory) {
+    return ItemPool({cost: _cost, drop1: _drop1, drop2: _drop2, drop3: _drop3});
+  }
 
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,14 +5,23 @@
 // Runtime Environment's members available in the global scope.
 const hre = require('hardhat');
 
+// async function main() {
+
+//   const Ethemerals = await hre.ethers.getContractFactory('Ethemerals');
+//   const ethemerals = await Ethemerals.deploy('https://api.ethemerals.com/api/', "0x169310e61e71ef5834ce5466c7155d8a90d15f1e"); // RANDOM ADDRESS
+
+// 	await ethemerals.deployed();
+
+// 	console.log('ethemerals deployed to:', ethemerals.address);
+// }
+
 async function main() {
+	const IntoTheWilds = await hre.ethers.getContractFactory('IntoTheWilds');
+	const intoTheWilds = await IntoTheWilds.deploy('0x169310e61e71ef5834ce5466c7155d8a90d15f1e'); // RANDOM ADDRESS
 
-  const Ethemerals = await hre.ethers.getContractFactory('Ethemerals');
-  const ethemerals = await Ethemerals.deploy('https://api.ethemerals.com/api/', "0x169310e61e71ef5834ce5466c7155d8a90d15f1e"); // RANDOM ADDRESS
+	await intoTheWilds.deployed();
 
-	await ethemerals.deployed();
-
-	console.log('ethemerals deployed to:', ethemerals.address);
+	console.log('wilds deployed to:', intoTheWilds.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
